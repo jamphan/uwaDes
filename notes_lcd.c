af6e1d5b33bd4ac1a1944eaaf50c67f09e40249c
Jamie Phan <jme.ph2@gmail.com>
2016-11-01 13:09:54 +0800

    add

---
+++ notes_lcd.c
@@ -0,0 +1,45 @@
+//LCD Control notes
+//-----------------------------------------------------------------------------
+//LCD control
+
+//Set the text cursor position to (row, col). always returns 0
+int LCDSetPos(int row, int col)
+
+//print formatted string to LCD and refresh LCD. Cursor position is updated. always returns 0
+int LCDPrintf(const char * format, ...)
+
+//clear the LCD display and all display buffers (turn it black). always return 0
+int LCDClear(void)
+
+
+//-------------------------------------
+//Example
+
+//init LCD
+LCDSetPos(10, 10); //set cursor to 10,10
+
+//program variables
+int encoderReading,
+int lcdcount = 0;
+
+while(1) {
+
+	char *encodingDisplay;
+
+	//Get sensor value
+	encoderReading = ENCODERead(motorNo);
+
+	//format sensor reading to string and then print
+	asprintf(&encodingDisplay, "%d", encoderReading);
+	LCDPrintf("Encoder Value: %s", encodingDisplay);
+	lcdcount++;
+
+	//clear screen if we are printing too many lines
+	if(lcdcount>10) {
+		LCDClear();
+		lcdcount=0;
+	}
+
+	free(encodingDisplay)
+}
+
---
+++ notes_motorControl.c
@@ -0,0 +1,35 @@
+//Motor Control Notes
+#include <eyebot.h>
+#include <stdio.h>
+
+//-----------------------------------------------------------------------------
+//Timer functions
+
+//adds a function to a 1 1Khz/scale timer. i.e executes fct every 1ms*scale
+//example: OSAttachTimer(1000/freq, &onOffController); //freq in Hz
+TIMER OSAttachTimer(int scale, void(*)void fct);
+
+//-----------------------------------------------------------------------------
+//motor control
+//Gives an encoder value from the specified encored, motorNo (1-4)
+//The difference in encoding divided by the sampling frequency should give 
+//motor velocity
+int ENCODERead( int motorNo ){}
+	encode = ENCODERead(motorNo);
+	if (encode == 1) {
+		printf("Error: motor\n");
+	}
+
+//Set the motor, specified by motorNo (1-4), to run at speed specified by motorSpeed
+//motorSpeed is given in absolute values [-100, +100]
+//returns error?
+int MOTORDriveRaw( int motorNo, int motorSpeed ){}
+	err = MOTORDriveRaw(motorNo, motorSpeedRaw);
+	if(err){
+		printf("Error: motor\n");
+	}
+
+//Similar to MOTORDriveRaw, however takes input as a relative percentage measure of speed
+//returns error?
+int MOTORDrive ( int motorNo, int motorSpeedRel ){}
+
