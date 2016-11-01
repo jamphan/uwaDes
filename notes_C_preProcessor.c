9ef4f6758c2489583656eaa31f46ac5d4f134184
Jamie Phan <jme.ph2@gmail.com>
2016-11-01 14:09:33 +0800

    add

---
+++ notes_C_preProcessor.c
@@ -0,0 +1,13 @@
+/*
+Short notes.
+	* implements the macro language to transform C programs before they are compiled
+	
+*/
+
+
+/* ----------------------------------------------------------------------------
+ * Useful preprocessors commands
+ * ----------------------------------------------------------------------------*/
+        
+#define MAX(a,b) ((a) > (b) ? a : b)
+#define MIN(a,b) ((a) < (b) ? a : b)
