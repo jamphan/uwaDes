0344eeefb100d9d1905031908b5759a1356f20b5
Jamie Phan <jme.ph2@gmail.com>
2016-11-02 00:26:03 +0800

    add

--- notes_imag_notes.c
+++ notes_imag_notes.c
@@ -7,21 +7,22 @@
 *//* ----------------------------------------------------------------------------
  * DATA TYPES
  * ----------------------------------------------------------------------------*/
-/*
-	int COLOR[3*pixels]
-		* array
-		* each pixel has 3 values: r, g, b <-- in that order
-		* E.g.
-			COLOR example = [ 125 0 240 32 36 25  76 .. ];
-							   ^  ^  ^   ^  ^  ^  ^
-							   |__|__|   |__|__|  |____ 'r' value of pixel 3
-							      |			|
-							   Pixel 1   Pixel 2
 
-		* Iterating example (note that we init 'px' to zero!)
+	// BYTE
+	// 	* array
+	// 	* each pixel has 3 values: r, g, b <-- in that order
+	//	* THUS size of BYTE array = N_PIXELS * 3;
+	// 	* E.g.
+	// 		COLOR example = [ 125 0 240 32 36 25  76 .. ];
+	// 						   ^  ^  ^   ^  ^  ^  ^
+	// 						   |__|__|   |__|__|  |____ 'r' value of pixel 3
+	// 						      |			|
+	// 						   Pixel 1   Pixel 2
+
+	// 	* Iterating example (note that we init 'px' to zero!)
 		for (int px = 0; px < N_PIXELS; px++) {
 			col[3*i]     = rBit;
 			col[3*i + 1] = gBit;
 			col[3*i + 2] = bBit;
 		}
-*/
+
